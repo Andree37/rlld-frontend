af5873ddd52f214e82832c60b3dd537b444a2263
Andre <33375224+Andree37@users.noreply.github.com>
2022-05-05 18:23:40 +0100

    remove unwanted import

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Box, ChakraProvider, Image, Stack } from '@chakra-ui/react';
+import { Box, Image, Stack } from '@chakra-ui/react';
 import logo from './assets/logo.svg';
 import Styleguide from './Styleguide';
 import Home from './components/molecules/GenerateURL';
